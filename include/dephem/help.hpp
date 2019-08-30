7aaab1000092cb4818638398fa73994db320948d
Maksim Philippov <vetrume@gmail.com>
2019-08-30 16:49:02 +0300

    Add ComponentsCount methods to dephem/help.hpp

--- include/dephem/help.hpp
+++ include/dephem/help.hpp
@@ -92,6 +92,23 @@ private:
 	Calculate(); // Запрет на создание объекта типа Calculate.
 };
 
+// Количество компонет для элемента.
+unsigned ComponentsCount(unsigned itemIndex)
+{   
+   return itemIndex > Other::TTmTDB ? 0 :
+                itemIndex == Other::TTmTDB ? 1 :
+                    itemIndex == Other::EARTH_NUTATIONS ? 2 : 3;   
+}
+
+// Количество компонент с учётом результата вычислений.
+unsigned ComponentsCount(unsigned itemIndex, unsigned calculationsResult)
+{
+    return calculationsResult == Calculate::POSITION ? 
+                                    ComponentsCount(itemIndex) :
+                calculationsResult == Calculate::STATE ? 
+                                        2 * ComponentsCount(itemIndex) : 0;
+}
+
 } } // namespace dph::help
 
 #endif // DEPHEM_HELP_HPP
